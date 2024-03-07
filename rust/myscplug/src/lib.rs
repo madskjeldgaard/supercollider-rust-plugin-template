d180f366b0fbdcc16ca7e36172753718a6991425
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2024-03-07 10:45:49 +0100

    fix: Pass Rust test

--- rust/myscplug/src/lib.rs
+++ rust/myscplug/src/lib.rs
@@ -28,7 +28,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = apply_gain(2, 2);
-        assert_eq!(result, 4);
+        let result = apply_gain(2.0, 2.0);
+        assert_eq!(result, 4.0);
     }
 }
